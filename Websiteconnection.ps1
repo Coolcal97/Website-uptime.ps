fd966630c57b9edc35af5d423a70ca3b9b699e4b
Calvon Spencer <Calvon98@gmail.com>
2026-05-09 01:37:44 -0400

    Add files via upload

---
+++ Websiteconnection.ps1
@@ -0,0 +1,18 @@
+﻿$url = "https://www.techstride.com" 
+$logPath = "C:\logs\Websitecheck.log"
+$smtpServer = "smtp.yourdomain.com"
+$from = "monitor@yourdomain.com"
+$to = "it@yourdomain.com"
+$subject = "Website Montior Alert" 
+
+$timestamp = get-date -Format "yyyy-MM-dd HH:mm:ss"
+
+try { $response = Invoke-WebRequest -Uri $url -UseBasicParsing -TimeoutSec 10
+$status = "UP ($($response.Statuscode))" 
+} catch { $status = "DOWN"}
+
+"$timestamp - $status" | Out-File -FilePath $logPath -Append
+
+if ($status -eq "DOWN") {
+$body = "Alert: $url is DOWN as of $timestamp" 
+Send-MailMessage -SmtpServer $smtpServer -From $from -To $to -Subject $subject -Body $body}
