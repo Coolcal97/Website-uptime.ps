6751acae9dd0b7af0ff9c8b863676bc7fdcfb5d8
Calvon Spencer <Calvon98@gmail.com>
2026-05-09 21:17:43 -0400

    Add files via upload

---
+++ Automationtask.ps1
@@ -0,0 +1,4 @@
+﻿$trigger = New-Scheduledtasktrigger -At 7pm -daily
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-File "C:\pc\Techstridenet.ps1"'
+$settings = New-ScheduledTaskSettingsSet
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Test Space" -Description "Tests connection with techstride.com" -Settings $settings
